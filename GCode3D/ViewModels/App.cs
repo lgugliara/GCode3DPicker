bd1e9b710b65e5c45a3ca6571540f4a9a532fece
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-26 15:46:25 +0100

    Refactor AppViewModel properties

--- GCode3D/ViewModels/App.cs
+++ GCode3D/ViewModels/App.cs
@@ -1,9 +1,26 @@
-﻿namespace GCode3D.ViewModels
+namespace GCode3D.ViewModels
 {
     public class AppViewModel : StandardViewModel
     {
-        public PickerViewModel PickerViewModel { get; } = new();
-        public PreviewViewModel PreviewViewModel { get; } = new();
-        public RunningViewModel RunningViewModel { get; } = new();
+        private PickerViewModel _PickerViewModel = new();
+        public PickerViewModel PickerViewModel
+        {
+            get => _PickerViewModel;
+            set => Set(ref _PickerViewModel, value);
+        }
+        
+        private PreviewViewModel _PreviewViewModel = new();
+        public PreviewViewModel PreviewViewModel
+        {
+            get => _PreviewViewModel;
+            set => Set(ref _PreviewViewModel, value);
+        }
+        
+        private RunningViewModel _RunningViewModel = new();
+        public RunningViewModel RunningViewModel
+        {
+            get => _RunningViewModel;
+            set => Set(ref _RunningViewModel, value);
+        }
     }
 }
