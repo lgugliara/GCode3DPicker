ae8b742d561b6fa5b6c055ee620375a0aaa44d25
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-27 21:59:08 +0100

    Add FileInfoLengthFormatter class and bind file length and last write time in Preview.xaml

---
+++ GCode3D/Converters/FileInfoLengthFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace GCode3D.Converters
+{
+    public class FileInfoLengthFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is long length ? $"{length / 1024.0 :F2} KB" : "-";
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
