031b117511a90d4aea04f62e71fbf9e82a29e22e
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-28 13:23:12 +0100

    Renamed FileNameWithoutExtension converter and update references

---
+++ GCode3D/Converters/FileNameWithoutExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace GCode3D.Converters
+{
+    public class FileNameWithoutExtension : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is string fileName ? 
+                Path.GetFileNameWithoutExtension(fileName) :
+                value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
