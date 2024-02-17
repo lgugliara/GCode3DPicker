ce658ccde089149de3190c44640cc8565ab7ed34
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-17 12:33:52 +0100

    Refactor Use class to support implicit conversion

--- GCode3D/Components/Commons/Use.cs
+++ GCode3D/Components/Commons/Use.cs
@@ -1,71 +1,19 @@
-﻿using System.Collections;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿namespace GCode3D.Components;
 
-namespace GCode3D.Components
+public class Use<T>(T? initialValue = default) : StandardComponent
 {
-    public class Use<T> : ObservableObject
+    private T? _Current = initialValue;
+    public T? Current
     {
-        private T? _value;
-        public T? Value
-        {
-            get => _value;
-            set => SetProperty(ref _value, value);
-        }
-
-        public static (T?, Action<Func<T?, T?>>) New(T? initialValue) =>
-            typeof(T) is IEnumerable ?
-                UseCollection<T>.New(initialValue) :
-                UseSingle<T>.New(initialValue);
+        get => _Current;
+        set => Set(ref _Current, value);
     }
 
-    public class UseSingle<T> : ObservableObject
-    {
-        private T? _value;
-        public T? Value
-        {
-            get => _value;
-            set => SetProperty(ref _value, value);
-        }
+    // Implicit Conversion to T
+    public static implicit operator T?(Use<T> use) => 
+        use.Current;
 
-        public static (T?, Action<Func<T?, T?>>) New(T? initialValue)
-        {
-            var _instance =
-                new UseSingle<T?>()
-                {
-                    _value = initialValue
-                };
-            var setter = new RelayCommand<T?>(x => _instance.Value = x);
-
-            return (
-                _instance.Value, 
-                (x) => setter.Execute(x(_instance.Value))
-            );
-        }
-    }
-
-    public class UseCollection<T> : ObservableObject
-    {
-        private T? _value;
-        public T? Value
-        {
-            get => _value;
-            set => SetProperty(ref _value, value);
-        }
-
-        public static (T?, Action<Func<T?, T?>>) New(T? initialValue)
-        {
-            var _instance =
-                new UseCollection<T?>()
-                {
-                    _value = initialValue
-                };
-            var setter = new RelayCommand<T?>(x => _instance.Value = x);
-
-            return (
-                _instance.Value, 
-                (x) => setter.Execute(x(_instance.Value))
-            );
-        }
-    }
+    // Implicit Conversion from T
+    public static implicit operator Use<T>(T? value) => 
+        new(value);
 }
