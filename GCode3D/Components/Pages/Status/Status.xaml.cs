6a62a696ec0ceff05ec584d534ac3f7a6d8d30d7
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-17 16:54:00 +0100

    Add StatusControl and related components

---
+++ GCode3D/Components/Pages/Status/Status.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace GCode3D.Components
+{
+    public partial class StatusControl : UserControl
+    {
+        private new StatusPage? DataContext =>
+            (StatusPage)base.DataContext;
+
+        public StatusControl() =>
+            InitializeComponent();
+    }
+}
---
+++ GCode3D/Components/Pages/Status/StatusComponent.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace GCode3D.Components
+{
+    public class StatusPage : Use<StatusPage>, IPage
+    {
+        #region Properties
+
+        public PickerComponent? PickerComponent { get; set; } = 
+            new Use<PickerComponent>(new());
+
+        public PreviewComponent? PreviewComponent { get; set; } = 
+            new Use<PreviewComponent>(new());
+
+        #endregion
+
+        #region Getters
+        
+        public string Name =>
+            "Status";
+
+        #endregion
+
+        #region Contstructor
+
+        public StatusPage()
+        {
+            if(PickerComponent != null)
+                PickerComponent.OnSelect =
+                    new RelayCommand(() =>
+                    {
+                        if(
+                            PickerComponent != null && 
+                            PreviewComponent != null
+                        )
+                            PreviewComponent.Program = 
+                                new()
+                                {
+                                    File = PickerComponent.Picker?.Selection
+                                };
+                    });
+        }
+
+        #endregion
+    }
+}
