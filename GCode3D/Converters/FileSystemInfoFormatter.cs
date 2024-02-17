b75d4bb8fb2125af04306e68ce36edf8ebd54ca5
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-17 14:58:44 +0100

    Update file length formatter in XAML files

--- GCode3D/Converters/FileSystemInfoFormatter.cs
+++ GCode3D/Converters/FileSystemInfoFormatter.cs
@@ -1,12 +1,18 @@
 ﻿using System.Globalization;
+using System.IO;
 using System.Windows.Data;
 
 namespace GCode3D.Converters
 {
-    public class FileInfoLengthFormatter : IValueConverter
+    public class FileSystemInfoFormatter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            value is long length ? $"{length / 1024.0 :F2} KB" : string.Empty;
+            value switch
+            {
+                FileInfo File => $"{File.Length / 1024.0 :F2} KB",
+                DirectoryInfo Directory => $"{Directory?.GetFileSystemInfos()?.Length} elements",
+                _ => string.Empty
+            };
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
