8e1dee1e89d5657d8bafc5d3f58ef597782a872b
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-17 16:53:49 +0100

    Add PrepareControl and related components

---
+++ GCode3D/Components/Pages/Prepare/Prepare.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace GCode3D.Components
+{
+    public partial class PrepareControl : UserControl
+    {
+        private new PreparePage? DataContext =>
+            (PreparePage)base.DataContext;
+
+        public PrepareControl() =>
+            InitializeComponent();
+    }
+}
---
+++ GCode3D/Components/Pages/Prepare/PrepareComponent.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace GCode3D.Components
+{
+    public class PreparePage : Use<PreparePage>, IPage
+    {
+        #region Properties
+
+        public PickerComponent? PickerComponent { get; set; } = 
+            new Use<PickerComponent>(new());
+
+        public PreviewComponent? PreviewComponent { get; set; } = 
+            new Use<PreviewComponent>(new());
+
+        #endregion
+
+        #region Getters
+        
+        public string Name =>
+            "Prepare";
+
+        #endregion
+
+        #region Contstructor
+
+        public PreparePage()
+        {
+            if(PickerComponent != null)
+                PickerComponent.OnSelect =
+                    new RelayCommand(() =>
+                    {
+                        if(
+                            PickerComponent != null && 
+                            PreviewComponent != null
+                        )
+                            PreviewComponent.Program = 
+                                new()
+                                {
+                                    File = PickerComponent.Picker?.Selection
+                                };
+                    });
+        }
+
+        #endregion
+    }
+}
