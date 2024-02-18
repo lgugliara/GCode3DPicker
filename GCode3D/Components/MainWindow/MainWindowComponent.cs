f031502cdf270374858602dea18ce90de240baf5
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-18 13:14:52 +0100

    Refactor MainWindowComponent to use simplified syntax for setting CurrentPage

--- GCode3D/Components/MainWindow/MainWindowComponent.cs
+++ GCode3D/Components/MainWindow/MainWindowComponent.cs
@@ -8,20 +8,15 @@ namespace GCode3D.Components
         public SidebarComponent? SidebarComponent { get; set; } = 
             new Use<SidebarComponent>(new());
 
-        public UserControl? CurrentPage { get; set; }
+        public UserControl? CurrentPage { get; set; } =
+            new Use<UserControl>(new());
 
         public RunningComponent? RunningComponent { get; set; } =
             new Use<RunningComponent>(new());
 
         public MainWindowComponent()
         {
-            SidebarComponent = new()
-            {
-                OnSelect = new RelayCommand<UserControl>((UserControl? page) =>
-                {
-                    CurrentPage = page;
-                })
-            };
+            SidebarComponent.OnSelect = new RelayCommand<UserControl>((UserControl? page) => CurrentPage = page);
             CurrentPage = SidebarComponent?.Pages?.FirstOrDefault();
         }
     }
