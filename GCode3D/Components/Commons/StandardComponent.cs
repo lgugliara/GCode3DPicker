e2771e5a2c6c8309d3aff0a76d4c9e09d51a054a
Lorenzo Gugliara <guguxd@hotmail.it>
2024-02-18 13:15:04 +0100

    Refactor OnPropertyChanged method and Set method in StandardComponent.cs

--- GCode3D/Components/Commons/StandardComponent.cs
+++ GCode3D/Components/Commons/StandardComponent.cs
@@ -9,12 +9,10 @@ namespace GCode3D.Components
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName]string info = "")
-        {
+        protected void OnPropertyChanged([CallerMemberName]string info = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
-        }
 
-        protected bool Set<T>(ref T backingField, T value, [CallerMemberName]string propertyName = "")
+        protected bool Set<T>(ref T? backingField, T? value, [CallerMemberName]string propertyName = "")
         {
             if (Equals(backingField, value))
                 return false;
@@ -25,6 +23,7 @@ namespace GCode3D.Components
         }
 
         #endregion
+
         #region IDisposable
 
         public virtual void Dispose() =>
